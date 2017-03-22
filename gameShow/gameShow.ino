a51eb4d477c5980c7816fae4f6a41a781c3d9e99
edat44 <edat444@gmail.com>
2017-03-22 17:36:20 -0400

    started commented, and tweaked buzzer underlying code

--- gameShow/gameShow.ino
+++ gameShow/gameShow.ino
@@ -2,14 +2,15 @@
 
 //Overarching game structure
 enum GAME_STATE {
-  question, //Waiting for contestant answer input
-  answer,
-  pause,
+  question, //Waiting for contestant to buzz in (using interrupts)
+  answer,   //Waiting for 'correct answer' or 'wrong answer' button to be pressed
+  pause,    //Waiting for 'start next question button' to be pressed, nothing else happens in this state
   gameOver
 };
+volatile GAME_STATE gameState = pause; //Start off in the pause state
 
 const int DELAY = 5; //Delay in milliseconds between iteration of the main loop
-const int PLAYERS = 2;
+const int PLAYERS = 2; //Number of contestants playing
 
 /*PIN CONSTANTS*/
 const int P1_BUTTON = 2;
@@ -21,6 +22,7 @@ const int P2_BUZZ_LED = 7;
 const int P1_SCORE_LEDS[] = {5, 6};
 const int P2_SCORE_LEDS[] = {9, 10};
 
+//Combines All Score LEDs into a single multidimensional array for easier processing
 const int SCORE_LEDS[2][2] = {
   {P1_SCORE_LEDS[0], P1_SCORE_LEDS[1]},
   {P2_SCORE_LEDS[0], P2_SCORE_LEDS[1]}
@@ -29,23 +31,24 @@ const int SCORE_LEDS[2][2] = {
 const int CORRECT_ANSWER_BUTTON = 8;
 const int WRONG_ANSWER_BUTTON = 11;
 const int POINT_VALUE_POT = A0;
-/*END PIN CONSTANTS*/
 
 const int BUZZER = 12;
 const int START_BUTTON = 13;
+/*END PIN CONSTANTS*/
 
-const int QUESTION_TIME = 5000;
+const int QUESTION_TIME = 5000; //Length of time (in milliseconds) that contestants get to answer each question
 
-//Variables to be changed in interrupts
-volatile GAME_STATE gameState = pause;
-int buzzedInPlayer;
-int points[2];
+volatile int buzzedInPlayer; //Keep track of player that buzzed in for the last question 
+int points[2]; //Keep track of points for both players
 
-unsigned long buzzLength = 0;
-unsigned long buzzLength2 = 0;
-unsigned long buzzDelay = 0;
-unsigned long buzzTone = 0;
+/*BUZZER VARIABLES*/
+volatile unsigned long buzzLength = 0; //Length of time (in milliseconds) for buzzer to buzz
+volatile unsigned long buzzRepeat = 0;
+volatile unsigned long buzzDelay = 0;
+volatile unsigned long buzzTone = 0;
 volatile unsigned long buzzTimer = 0;
+/*END BUZZER VARIABLE*/
+
 unsigned long questionTimer = 0;
 
 void setup()
@@ -92,9 +95,9 @@ void loop()
       newTime = millis();
       if (newTime >= questionTimer + QUESTION_TIME) {
         gameState = pause;
-        buzzTone = 750;
+        buzzTone = 1000;
+        buzzLength = 750;
         buzzTimer = millis();
-        buzzLength = 1000;
         Serial.println("Out of time!");
       }
       break;
@@ -107,8 +110,8 @@ void loop()
         if (correct) {
           points[buzzedInPlayer - 1] += pointValue;
           buzzLength = 500;
-          buzzTimer = millis();
           buzzTone = 1600;
+          buzzTimer = millis();
           Serial.print("Player ");
           Serial.print(buzzedInPlayer);
           Serial.println(" is correct!");
@@ -119,10 +122,10 @@ void loop()
         else {
           points[buzzedInPlayer - 1] = max(points[buzzedInPlayer - 1] - pointValue, 0);
           buzzLength = 120;
+          buzzRepeat = 1;
           buzzDelay = 100;
-          buzzLength2 = 120;
-          buzzTimer = millis();
           buzzTone = 400;
+          buzzTimer = millis();
           Serial.print("Player ");
           Serial.print(buzzedInPlayer);
           Serial.print(" is wrong!");
@@ -175,10 +178,9 @@ void loop()
     unsigned long newTime = millis();
     if ((newTime - buzzTimer) >= buzzLength) {
       noTone(BUZZER);
-      if (buzzLength2 > 0) {
+      if (buzzRepeat > 0) {
         delay(buzzDelay);
-        buzzLength = buzzLength2;
-        buzzLength2 = 0;
+        buzzRepeat--;
         buzzTimer = millis();
       }
       else
@@ -199,8 +201,8 @@ void playerTwoAnswer() {
 void buzzIn(int player) {
   if (gameState == question) {
     buzzTone = 800;
-    buzzTimer=millis();
     buzzLength = 750;
+    buzzTimer=millis();
     Serial.print("Player ");
     Serial.print(player);
     Serial.println(" buzzed in!");
